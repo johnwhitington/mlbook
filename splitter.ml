49d48bb9749c7bd4afa55b28a9d5918f05e45b81
John Whitington <john@coherentgraphics.co.uk>
2021-08-17 14:07:32 +0200

    First decent HTML split working

--- splitter.ml
+++ splitter.ml
@@ -8,12 +8,25 @@ let body_contents = soup $$ "body" |> R.first |> children
 let filenum = ref 0
 
 let open_a_file () =
-  Printf.printf "Writing html/%02i.html\n%!" !filenum;
-  open_out_bin (Printf.sprintf "html/%02i.html" !filenum)
+  let name = Printf.sprintf "html/split%02i.html" !filenum in
+    Printf.printf "Writing %s\n%!" name;
+    open_out_bin name
 
 let the_open_file = ref (open_a_file ())
 
+let contents_of_file filename =
+  let ch = open_in_bin filename in
+  let s = really_input_string ch (in_channel_length ch) in
+  close_in ch;
+  s
+
+let header = contents_of_file "html/header.html"
+let footer = contents_of_file "html/footer.html"
+
+let copy s = output_string !the_open_file s
+
 let () =
+  copy header;
   iter
     (fun node ->
        if 
@@ -23,8 +36,12 @@ let () =
        then
          begin
            filenum := !filenum + 1;
+           copy footer;
            close_out !the_open_file;
-           the_open_file := open_a_file ()
+           the_open_file := open_a_file ();
+           copy header
          end;
-       output_string !the_open_file (to_string node))
-    body_contents
+       copy (to_string node))
+    body_contents;
+    copy footer;
+    close_out !the_open_file
